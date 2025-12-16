ffba46d6316bcff8c100b1b24a26ad38959c3840
Gustavo de Oliveira Viana <gustavooliveira1x1@gmail.com>
2025-12-16 12:33:29 -0300

    tinha feito sem ler as instruções, corrigido

--- m.go
+++ m.go
@@ -1,30 +1,60 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
+type Taxas struct {
+	Base  string
+	Date  string
+	Rates map[string]float64
+}
+
 func main() {
-	tx := 2.5
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Digite o valor para ser convertido: ")
-	input, _ := reader.ReadString('\n')
-	cleanerValue, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
-	if err != nil {
-		fmt.Println("Valor invalido")
-		return
+	if len(os.Args) > 2 {
+
+		file, err := os.ReadFile("rates.json")
+
+		if err != nil {
+			panic(err)
+		}
+
+		var js Taxas
+
+		conversor := json.Unmarshal(file, &js)
+		if conversor != nil {
+			fmt.Println("Erro ao decodificar JSON")
+			return
+		}
+
+		in, err := strconv.ParseFloat(os.Args[1], 64)
+		if err != nil {
+			fmt.Println("Impossivel converter, reveja se a entrada foi um numero")
+			return
+		}
+
+		moeda := strings.ToUpper(os.Args[2])
+		tax, find := js.Rates[moeda]
+
+		if !find {
+			fmt.Println("Moeda não encontrada")
+			return
+		}
+
+		conv := calc(in, tax)
+		saida := fmt.Sprintf("%.2f", conv)
+		saida = strings.Replace(saida, ".", ",", 1)
+		fmt.Printf("Valor convertido:%s %s \n", moeda, saida)
+	} else {
+		fmt.Println("È preciso o valor e a moeda para conversão")
 	}
-	convertValue := calc(cleanerValue, tx)
-	fmt.Printf("Valor convetido:$%.2f\n", convertValue)
 }
 
 func calc(valor float64, tx float64) float64 {
-	imposto := valor * (tx / 100)
-	cabio := 5.50
-	conv := (valor - imposto) / cabio
+	conv := valor * tx
 	return conv
 }
